16c02f8c73b3405ff9e0ac6e20d6255d2870e1f0
Kenny5231 <124184507+Kenny5231@users.noreply.github.com>
2026-02-14 22:19:54 -0600

    uis

--- Web/Backend/src/Controllers/auth.js
+++ Web/Backend/src/Controllers/auth.js
@@ -1,5 +1,5 @@
 const bcrypt = require("bcrypt");
-const prisma = require("../prisma/client");
+const prisma = require("../config/database");
 
 //registrar usuario
 async function register(req, res) {
